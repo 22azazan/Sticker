9c89739eee8f480063676a5d09d5b4438ff3ed9f
Shin Ji Min <shinjm26@naver.com>
2023-03-15 20:14:00 +0900

    load img_list

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from PyQt5 import QtWidgets,QtCore
 from PyQt5.QtCore import * 
 from pynput import mouse
 import sys, time
+import cv2
+import os
 
 class MyApp(QWidget):
 
@@ -20,8 +22,20 @@ class MyApp(QWidget):
         self.resize(400, 200)
         self.show()
 
+    def resizeEvent(self, event): #창 사이즈 구하기
+        print(self.size())
+
+img_list = []    
 img_path = open("image_path.txt", "r", encoding="Utf-8").readline()
-print(img_path)
+
+for (root, directories, files) in os.walk(img_path):
+    directories.clear()
+    for file in files:
+        if ".png" in file or ".jpg" in file or ".gif" in file:
+            file_path = os.path.join(root, file)
+            img_list.append(file_path)
+            
+print(img_list)
 
 if __name__ == '__main__':
    app = QApplication(sys.argv)
