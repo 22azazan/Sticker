10f8c4d46363b072b306f648aead619d0ccadf97
Shin Ji Min <shinjm26@naver.com>
2023-03-15 19:29:09 +0900

    First Commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from PyQt5.QtWidgets import *
+from PyQt5 import QtWidgets,QtCore
+from PyQt5.QtCore import * 
+from pynput import mouse
+import sys, time
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('My First Application')
+        """ flag=QtCore.Qt.WindowFlags(QtCore.Qt.FramelessWindowHint)
+        self.setWindowFlags(flag) """
+        self.move(300, 300)
+        self.resize(400, 200)
+        self.show()
+
+
+if __name__ == '__main__':
+   app = QApplication(sys.argv)
+   ex = MyApp()
+   sys.exit(app.exec_())
