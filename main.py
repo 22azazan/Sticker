e7c90b23b8bc29d82653e98b1462a8ae0d11cc6c
Shin Ji Min <shinjm26@naver.com>
2023-03-19 02:43:08 +0900

    V 1.4 - resize image with window when scroll

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from PyQt5.QtWidgets import *
 from PyQt5 import QtWidgets,QtCore
 from PyQt5.QtCore import * 
-from PyQt5.QtGui import QPixmap, QImage, QFont, QWheelEvent
-import sys, time
-from PIL import Image
-import os
+from PyQt5.QtGui import *
+import os, sys
+
 
 class MyApp(QWidget):
 
@@ -13,11 +12,12 @@ class MyApp(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.old_size = self.size()
         self.img_num = 0
-        self.img = img_list[0]
 
-        try: self.pixmap = QPixmap(self.img)
+        self.old_size = self.size()
+        try: 
+            self.img = img_list[self.img_num]
+            self.pixmap = QPixmap(self.img)
         except: sys.exit("NO IMAGE")
 
         img_size = [self.pixmap.width(), self.pixmap.height()]
@@ -25,12 +25,6 @@ class MyApp(QWidget):
         n_w=self.pixmap.width()
         n_h=self.pixmap.height()
         print("n_w: {0}, n_h: {1}".format(n_w, n_h))
-        self.img_label = QLabel()
-        self.img_label.setPixmap(self.pixmap)
-        self.img_label.setScaledContents(True)
-        hbox = QHBoxLayout()
-        hbox.addWidget(self.img_label)
-        self.setLayout(hbox)
 
         self.setWindowTitle('My First Application')
 
@@ -41,45 +35,47 @@ class MyApp(QWidget):
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
         self.show()
 
+    def paintEvent(self, e) -> None:
+        self.pixmap = QPixmap(self.img)
+        # 창의 크기와 이미지의 크기 비율 계산
+        ratio = min(self.width() / self.pixmap.width(), self.height() / self.pixmap.height()) #0.625
+        # QPixmap 객체 크기 조정
+        new_size = self.pixmap.size() * ratio
+        print(new_size)
+        pixmap = self.pixmap.scaled(new_size)
+        # QPainter 객체 생성
+        painter = QPainter(self)
+        # QPainter로 이미지 그리기
+        painter.drawPixmap(int((self.width() - new_size.width()) / 2), int((self.height() - new_size.height()) / 2), pixmap)
+
     def mousePressEvent(self, event): # 마우스 클릭 이벤트
         if event.button() == QtCore.Qt.LeftButton: # 왼쪽 버튼이면
             self.dragPosition = event.globalPos() - self.frameGeometry().topLeft() # 드래그 위치 저장
             event.accept()
 
     def mouseMoveEvent(self, event): # 마우스 이동 이벤트
-        print(event.pos())
-
         if event.buttons() == QtCore.Qt.LeftButton: # 왼쪽 버튼이 눌려있으면
             self.move(event.globalPos() - self.dragPosition) # 드래그 위치만큼 창 이동
             event.accept()
 
-    
     def wheelEvent(self, e: QWheelEvent): #스크롤 시 사진 바꾸기
-        if e.angleDelta().y() > 0: #스크롤 업 -> 이전 사진
-            self.img_num -= 1
-            self.img = img_list[self.img_num]
-            try: 
-                self.pixmap = QPixmap(self.img)
-                self.img_label.setPixmap(self.pixmap)
-                print(self.img_num)
-            except: 
-                self.img_num += 1
-
-        elif e.angleDelta().y() < 0: #스크롤 다운 -> 다음 사진
-            self.img_num += 1
-            self.img = img_list[self.img_num]
-            try: 
-                self.pixmap = QPixmap(self.img)
-                self.img_label.setPixmap(self.pixmap)
-            except: 
-                self.img_num -= 1
-                print("No image")
+        
+        if e.angleDelta().y() > 0: self.img_num -= 1 #스크롤 업 -> 이전 사진
+        elif e.angleDelta().y() < 0: 
+            self.img_num += 1 #스크롤 다운 -> 다음 사진 
+            if self.img_num > len(img_list)-1 : self.img_num = 0         
         else: pass
 
-    def enterEvent(self, e):
-        self.setWindowOpacity(0.5)
-    def leaveEvent(self, e):
-         self.setWindowOpacity(1)
+        try:
+            self.img = img_list[self.img_num]
+            self.pixmap = QPixmap(self.img)
+        except: 
+            self.img_num -= 1
+            print("No image")
+
+        print(self.img_num)
+        print("\n")
+        self.update()
 
     def mouseDoubleClickEvent(self, e): #더블클릭하면 종료
         QtWidgets.qApp.quit()
